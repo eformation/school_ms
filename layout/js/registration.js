9826fbdbc762569a038a5dd7e7055f9fca71e2a8
ankur140290 <ankur140290@gmail.com>
2013-02-06 01:04:52 +0530

    Adding script for refistration form

--- layout/js/registration.js
+++ layout/js/registration.js
@@ -1,18 +1,42 @@
 /*
+ * Declaring global variables
+ */
+opentab = "student";
+
+/*
+ * Functions handling display of registration fields as per the openned tab
+ */
+
+function school_tab()
+{
+    $('input#idno').parent().parent().addClass("hide");
+}
+
+function teacher_tab()
+{
+    $('input#idno').parent().parent().removeClass("hide");
+}
+
+function student_tab()
+{
+    $('input#idno').parent().parent().removeClass("hide");
+}
+
+/*
  * Set local storage
  */
 
 if(localStorage.registration)
 {
-    option = localStorage.registration;
+    opentab = localStorage.registration;
     $(".tab-pane").removeClass('active');
-    $('div#'+option).addClass('active');
+    $('div#'+opentab).addClass('active');
     $(".nav li").removeClass('active');
-    $('li.'+option).addClass('active');
+    $('li.'+opentab).addClass('active');
 }
 else
 {
-    localStorage.registration = "Student";
+    localStorage.registration = "student";
 }
 
 /*
@@ -21,4 +45,24 @@ else
 
 $('.nav a').live('click',function(){
    localStorage.registration = $(this).attr('href').substr(1); 
+});
+
+/*
+ * Code to handle display of fields on page load
+ */
+if(opentab == 'school')
+    school_tab();
+else if(opentab == 'teacher')
+    teacher_tab();
+else
+    student_tab();
+
+/*
+ * Code to handle code display when switching b/w tabs
+ */
+
+$('.nav li a').live('click',function() {
+   var function_call_name = $(this).parent().attr('class');
+   function_call_name = function_call_name +"_tab";
+   function_call_name();
 });
